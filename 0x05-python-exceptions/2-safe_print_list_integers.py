5c9c091855f5a03b686ee391ae9ffd0f66503617
Onkarabile <letlapeonka@gmail.com>
2023-06-26 13:38:12 -0700

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+ ret = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            ret += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (ret)
+
